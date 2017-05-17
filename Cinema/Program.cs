08869629d657a254af80a9ec46f95c98600dcaf5
Michael E Miles <michael@miles-me.net>
2017-05-17 16:36:24 -0500

    Complete python conversion to C#

--- Cinema/Program.cs
+++ Cinema/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Cinema
 {
@@ -10,7 +7,40 @@ namespace Cinema
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Cinema");
+            var films = new Dictionary<string, List<int>>()
+            {
+                {"finding dory", new List<int>() { 3, 5 } },
+                {"bourne", new List<int>() { 18, 5 } },
+                {"tarzan", new List<int>() {15,5 } },
+                {"ghost busters", new List<int>() {12,5 } }
+            };
+
+            while (true)
+            {
+                Console.Write("What film would you like to watch?: ");
+                var choice = Console.ReadLine().ToLower();
+
+                if (films.ContainsKey(choice))
+                {
+                    Console.Write("How old are you?: ");
+                    var age = Convert.ToInt32(Console.ReadLine());
+
+                    if (age >= films[choice][0])
+                    {
+                        if (films[choice][1] > 0)
+                        {
+                            Console.WriteLine("Enjoy the film!");
+                            films[choice][1] -= 1;
+                        }
+                        else
+                            Console.WriteLine("Sorry we are sold out!");
+                    }
+                    else
+                        Console.WriteLine("You are too young to see that film!");
+                }
+                else
+                    Console.WriteLine("We don't have that film...");
+            }
         }
     }
 }
